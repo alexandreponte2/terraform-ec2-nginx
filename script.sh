15b5e89c67281685f2092d4622d8fffaedbd10d5
alexandreponte2 <79060274+alexandreponte2@users.noreply.github.com>
2022-05-16 16:49:42 -0300

    update sudo

--- script.sh
+++ script.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 sudo apt-get update
 sudo apt install -y nginx
+sudo su
 sudo echo "<h1>Hello World from the other side, i am at $(hostname -f)</h1>" > /var/www/html/index.nginx-debian.html
 sudo systemctl restart nginx
 sudo systemctl status nginx
