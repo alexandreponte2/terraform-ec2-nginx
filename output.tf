56c8343d1bc52967d5d46fe00468d3a40c7791b7
alexandreponte1 <alexandre.s.ponte@concrete.com.br>
2022-11-29 11:14:50 -0300

    comentarios sobre configurar profile e imagem ec2

--- output.tf
+++ output.tf
@@ -1,3 +1,5 @@
 output "dns" {
   value = aws_instance.web1.public_dns
 }
+
+# acessar apenas com http, já que é apenas um teste.
--- vars.tf
+++ vars.tf
@@ -1,13 +1,22 @@
 variable "amis" {
   type = map(string)
-
+#Altere a imagem conforme a região
+# aqui estamos usando a umagem do ubuntu devido ao script.ssh
   default = {
-    "us-east-1" = "ami-09e67e426f25ce0d7"
-    "us-east-2" = "ami-09e67e426f25ce0d7"
+    "us-east-1" = "ami-08c40ec9ead489470"
+    "us-east-2" = "ami-08c40ec9ead489470"
   }
 }
 
 variable "cdirs_acesso_remoto" {
   type    = list(string)
-  default = ["Seu-IP-Aqui/32"]
+  default = ["seu-ip-aqui/32"]
+}
+
+#Recomendo configurar um profile
+#https://docs.aws.amazon.com/cli/latest/userguide/cli-configure-profiles.html
+variable "aws_profile" {
+  type        = string
+  description = " "
+  default     = "playground"
 }
